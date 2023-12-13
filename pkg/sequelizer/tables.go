807fcd03475c813b8c1a60ec8e91437a1f083296
Omar Rafat <omarrafat60@gmail.com>
2023-12-13 16:42:29 +0300

    Started an incomplete implementation of postgres table DDL creation

--- pkg/sequelizer/tables.go
+++ pkg/sequelizer/tables.go
@@ -2,6 +2,9 @@ package sequelizer
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"strings"
 
 	"github.com/Okira-E/patchi/pkg/utils"
 )
@@ -13,7 +16,8 @@ func generateSqlForTables(firstDb *sql.DB, secondDb *sql.DB, dialect string, ent
 	if dialect == "mysql" || dialect == "mariadb" {
 		ret = generateSqlForTablesMysql(firstDb, secondDb, entityType, entityName, status)
 	} else if dialect == "postgres" || dialect == "cockroachdb" {
-		ret = generateSqlForTablesPostgres(firstDb, secondDb, entityType, entityName, status)
+		utils.AbortTui("UNIMPLEMENTED")
+		// ret = generateSqlForTablesPostgres(firstDb, secondDb, entityType, entityName, status) // Incomplete
 	}
 
 	return ret
@@ -39,8 +43,6 @@ func generateSqlForTablesMysql(firstDb *sql.DB, secondDb *sql.DB, entityType str
 		ret += ";"
 	} else if status == "deleted" {
 		ret = "DROP TABLE IF EXISTS `" + entityName + "`;"
-	} else if status == "modified" {
-		utils.AbortTui("UNIMPLEMENTED")
 	}
 
 	return ret
@@ -50,7 +52,86 @@ func generateSqlForTablesMysql(firstDb *sql.DB, secondDb *sql.DB, entityType str
 func generateSqlForTablesPostgres(firstDb *sql.DB, secondDb *sql.DB, entityType string, entityName string, status string) string {
 	var ret string
 
-	utils.AbortTui("UNIMPLEMENTED")
+	if status == "created" {
+		// Query to retrieve column and key information
+		rows, err := firstDb.Query(`
+		SELECT columns.column_name, data_type, character_maximum_length, is_nullable,
+		       column_default, columns.ordinal_position,
+		       constraint_type, constraint_name, referenced_table_name, referenced_column_name
+		FROM information_schema.columns
+		LEFT JOIN information_schema.key_column_usage
+		ON columns.column_name = key_column_usage.column_name
+		WHERE table_name = $1
+		ORDER BY ordinal_position;
+	`, entityName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		// Collect column and key information
+		var columns []string
+		var primaryKeyCols []string
+		var foreignKeyCols []string
+
+		for rows.Next() {
+			var columnName, dataType, isNullable, columnDefault, constraintType, constraintName sql.NullString
+			var characterMaximumLength, ordinalPosition sql.NullInt64
+			var referencedTableName, referencedColumnName sql.NullString
+
+			if err := rows.Scan(&columnName, &dataType, &characterMaximumLength, &isNullable,
+				&columnDefault, &ordinalPosition, &constraintType, &constraintName,
+				&referencedTableName, &referencedColumnName); err != nil {
+				log.Fatal(err)
+			}
+
+			columnDef := fmt.Sprintf("%s %s", columnName.String, dataType.String)
+			if characterMaximumLength.Valid {
+				columnDef += fmt.Sprintf("(%d)", characterMaximumLength.Int64)
+			}
+			if isNullable.String == "NO" {
+				columnDef += " NOT NULL"
+			}
+			if columnDefault.Valid {
+				columnDef += fmt.Sprintf(" DEFAULT %s", columnDefault.String)
+			}
+
+			columns = append(columns, columnDef)
+
+			if constraintType.Valid {
+				if constraintType.String == "PRIMARY KEY" {
+					primaryKeyCols = append(primaryKeyCols, columnName.String)
+				} else if constraintType.String == "FOREIGN KEY" {
+					foreignKeyCols = append(foreignKeyCols, fmt.Sprintf("%s REFERENCES %s(%s)",
+						columnName.String, referencedTableName.String, referencedColumnName.String))
+				}
+			}
+		}
+
+		// Check for errors from iterating over rows.
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		// Create the CREATE TABLE statement
+		createTableStatement := fmt.Sprintf("CREATE TABLE %s (\n\t%s", entityName, strings.Join(columns, ",\n\t"))
+
+		// Add primary key constraint
+		if len(primaryKeyCols) > 0 {
+			createTableStatement += fmt.Sprintf(",\n\tPRIMARY KEY (%s)", strings.Join(primaryKeyCols, ", "))
+		}
+
+		// Add foreign key constraints
+		if len(foreignKeyCols) > 0 {
+			createTableStatement += fmt.Sprintf(",\n\t%s", strings.Join(foreignKeyCols, ",\n\t"))
+		}
+
+		createTableStatement += "\n);"
+
+		fmt.Println(createTableStatement)
+	} else if status == "deleted" {
+		ret = "DROP TABLE IF EXISTS `" + entityName + "`;"
+	}
 
 	return ret
 }
